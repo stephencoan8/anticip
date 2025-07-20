4c226b89cf012ad43e4ed410811a4107a8620aa6
Stephen <stephencoan8@gmail.com>
2025-07-20 11:23:34 -0700

    Added monthly listener info to verify API is working

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+# Load environment variables
+load_dotenv()
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+# Set up Spotipy
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
 @app.route('/')
 def home():
-    return 'Hello, Antici App!'
+    # Example: Get artist data for The Beatles
+    artist = sp.artist("3WrFJ7ztbogyGnTHbHJFl2")
+    return f"Artist: {artist['name']}, Monthly Listeners: {artist['followers']['total']}"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
